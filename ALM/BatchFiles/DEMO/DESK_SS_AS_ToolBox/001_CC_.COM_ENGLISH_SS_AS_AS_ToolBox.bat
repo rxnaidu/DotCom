191564b3adb31a0703509351fcd7814d5eb5a5d6
hjj446 <hjj446@ftr.com>
2017-03-08 15:40:27 -0600

    Updated ALM bat file for DEMO

--- ALM/BatchFiles/DEMO/DESK_SS_AS_ToolBox/001_CC_.COM_ENGLISH_SS_AS_AS_ToolBox.bat
+++ ALM/BatchFiles/DEMO/DESK_SS_AS_ToolBox/001_CC_.COM_ENGLISH_SS_AS_AS_ToolBox.bat
@@ -2,4 +2,4 @@
 
 cd c:\Development\Robot\Dotcom
 
-call robot -d ALM\TestResults\001_CC_.COM_ENGLISH_SS_AS_AS_ToolBox Tests\DESK_SS_AS_ToolBox\001_CC_.COM_ENGLISH_SS_AS_AS_ToolBox.robot
+call robot -d ALM\TestResults\001_CC_.COM_ENGLISH_SS_AS_AS_ToolBox Tests\DEMO\DESK_SS_AS_ToolBox\001_CC_.COM_ENGLISH_SS_AS_AS_ToolBox.robot
--- ALM/BatchFiles/DEMO/DSK_NSS_RES_SHOP_BUNDLES/001_CC_.COM_ENGLISH_NSS_Res_Shop_Bundles.bat
+++ ALM/BatchFiles/DEMO/DSK_NSS_RES_SHOP_BUNDLES/001_CC_.COM_ENGLISH_NSS_Res_Shop_Bundles.bat
@@ -2,4 +2,4 @@
 
 cd c:\Development\Robot\Dotcom
 
-call robot -d ALM\TestResults\001_CC_.COM_ENGLISH_NSS_Res_Shop_Bundles Tests\DSK_NSS_RES_SHOP_BUNDLES\001_CC_.COM_ENGLISH_NSS_Res_Shop_Bundles.robot
+call robot -d ALM\TestResults\001_CC_.COM_ENGLISH_NSS_Res_Shop_Bundles Tests\DEMO\DSK_NSS_RES_SHOP_BUNDLES\001_CC_.COM_ENGLISH_NSS_Res_Shop_Bundles.robot

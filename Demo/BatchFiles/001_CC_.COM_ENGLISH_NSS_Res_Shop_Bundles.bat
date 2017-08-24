7a6b940708fffd5b89c8709308363dedd23e24bf
hjj446 <hjj446@ftr.com>
2017-08-24 12:21:57 -0500

    clean it folders

--- Demo/BatchFiles/001_CC_.COM_ENGLISH_NSS_Res_Shop_Bundles.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cd c:\Development\Robot\Dotcom
-
-call robot -d ALM\TestResults\001_CC_.COM_ENGLISH_NSS_Res_Shop_Bundles Tests\DEMO\DSK_NSS_RES_SHOP_BUNDLES\001_CC_.COM_ENGLISH_NSS_Res_Shop_Bundles.robot
--- Demo/BatchFiles/001_CC_.COM_ENGLISH_SS_AS_AS_ToolBox.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cd c:\Development\Robot\Dotcom
-
-call robot -d ALM\TestResults\001_CC_.COM_ENGLISH_SS_AS_AS_ToolBox Tests\DEMO\DESK_SS_AS_ToolBox\001_CC_.COM_ENGLISH_SS_AS_AS_ToolBox.robot
